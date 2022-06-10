12c4b6459628856ff41284d6997a5ad0686d2133
Anna <annaruma2005@gmail.com>
2022-06-10 11:24:19 +0300

    committ

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('blog/', include('blog.urls', namespace='blog')),
+    path('', include('blog.urls', namespace='blog')),
 ]
